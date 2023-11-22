97d6afd808da1d99409bd481d0a3452e10203378
Neal Jayaraman <nealtho64@gmail.com>
2023-11-22 10:22:26 -0500

    find header

--- WebScrap.py
+++ WebScrap.py
@@ -2,4 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file: 
     content = html_file.read()
-    print(content)
+    
+    soup = BeautifulSoup(content, 'lxml')
+    tags = soup.find('h5')
+    print (tags)
